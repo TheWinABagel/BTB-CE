2c1b041ac3b93917f3f9beb6ccc67aafd95d0b08
Ash (Bagel) <ash@rjohnston.net>
2024-10-30 15:18:02 -0500

    remove debug printing

--- src/main/java/dev/bagel/btb/mixin/ItemInWorldManagerMixin.java
+++ src/main/java/dev/bagel/btb/mixin/ItemInWorldManagerMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Debug(export = true)
 @Mixin(ItemInWorldManager.class)
 public abstract class ItemInWorldManagerMixin {
 
@@ -18,17 +17,17 @@ public abstract class ItemInWorldManagerMixin {
     private void btb$onItemUseFirstHook(EntityPlayer entityPlayer, World world, ItemStack itemStack, int par4, int par5, int par6, int par7, float par8, float par9, float par10, CallbackInfoReturnable<Boolean> cir) {
         Item item = itemStack != null ? itemStack.getItem() : null;
         if (item instanceof ItemBuildCraft itemBc && itemBc.onItemUseFirst(itemStack, entityPlayer, world, par4, par5, par6, par7, par8, par9, par10)) {
-            System.out.println("activate block ItemInWorldManager onItemFirstUse: TRUE");
+//            System.out.println("activate block ItemInWorldManager onItemFirstUse: TRUE");
             cir.setReturnValue(true);
         } else {
-            System.out.println("activate block ItemInWorldManager onItemFirstUse: FALSE");
+//            System.out.println("activate block ItemInWorldManager onItemFirstUse: FALSE");
         }
     }
 
     @Redirect(method = "activateBlockOrUseItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/EntityPlayer;isSneaking()Z"))
     private boolean btb$shouldPassSneakClickToBlockHook(EntityPlayer player, EntityPlayer entityPlayer, World world, ItemStack stack, int i, int j, int k, int par7, float par8, float par9, float par10) {
         boolean shouldNotPass = player.isSneaking() || (player.getHeldItem() != null && player.getHeldItem().getItem() instanceof ItemBuildCraft itemBc && !itemBc.shouldPassSneakingClickToBlock(world, i, j, k));
-        System.out.println("activate block ItemInWorldManager isSneaking: (inverted) " + shouldNotPass);
+//        System.out.println("activate block ItemInWorldManager isSneaking: (inverted) " + shouldNotPass);
         return shouldNotPass;
     }
 }
